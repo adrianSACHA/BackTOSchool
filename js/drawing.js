4dff8311cf19c8c5e8b849cb6b1c92d4ffbb43a8
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-03-22 23:28:02 +0100

    test

--- js/drawing.js
+++ js/drawing.js
@@ -54,3 +54,44 @@ canvas.addEventListener('mouseup', (e) => {
 });
 
 canvas.addEventListener('mousemove', draw);
+
+
+// const canvas = document.getElementById("drawing-board");
+// const toolbar = document.getElementById("toolbar");
+// const ctx = canvas.getContext("2d");
+
+// canvas.width = innerWidth - canvas.offsetLeft;
+// canvas.height = innerHeight - canvas.offsetTop;
+
+// let isPainting, lineWidth, startX, startY;
+
+// toolbar.addEventListener("click", (e) => {
+//   if (e.target.id === "clear") ctx.clearRect(0, 0, canvas.width, canvas.height);
+// });
+
+// toolbar.addEventListener("change", (e) => {
+//   if (e.target.id === "stroke") ctx.strokeStyle = e.target.value;
+//   if (e.target.id === "lineWidth") lineWidth = e.target.value;
+// });
+
+// const draw = (e) => {
+//   if (!isPainting) return;
+//   ctx.lineWidth = lineWidth;
+//   ctx.lineCap = "round";
+//   ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY);
+//   ctx.stroke();
+// };
+
+// canvas.addEventListener("mousedown", (e) => {
+//   isPainting = true;
+//   startX = e.clientX;
+//   startY = e.clientY;
+// });
+
+// canvas.addEventListener("mouseup", (e) => {
+//   isPainting = false;
+//   ctx.stroke();
+//   ctx.beginPath();
+// });
+
+// canvas.addEventListener("mousemove", draw);
