9618dcf44d21cc8d9f30968a746a548fc37a737c
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-03-23 12:05:31 +0100

    moved slide-in function

--- js/script.js
+++ js/script.js
@@ -16,21 +16,3 @@ function showSlides(n) {
   dots.forEach((dot) => dot.classList.remove("active"));
   dots[slideIndex - 1].classList.add("active");
 }
-
-window.addEventListener('scroll', reveal);
-
-function reveal() {
-  const reveals = document.querySelectorAll('.reveal');
-
-  for (let i = 0; i < reveals.length; i++) {
-    const windowHeight = window.innerHeight;
-    const revealTop = reveals[i].getBoundingClientRect().top;
-    const revealPoint = 50;
-
-    if (revealTop < windowHeight - revealPoint) {
-      reveals[i].classList.add('slide-in');
-    } else {
-      reveals[i].classList.remove('slide-in');
-    }
-  }
-}
