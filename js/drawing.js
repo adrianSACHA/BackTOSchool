46697a7ad90c689a7d53192475bc041b277446c8
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-03-24 12:46:37 +0100

    finishing touches

--- js/drawing.js
+++ js/drawing.js
@@ -1,14 +1,11 @@
 const canvas = document.getElementById("drawing-board");
 const ctx = canvas.getContext("2d");
 
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-
-canvas.width = "559";
-canvas.height = "593";
+canvas.width = 559;
+canvas.height = 593;
 
 let isPainting = false;
-let lineWidth = "5";
+let lineWidth = 5;
 
 const draw = (e) => {
   if (!isPainting) {
@@ -19,23 +16,27 @@ const draw = (e) => {
   ctx.lineCap = "round";
   ctx.strokeStyle = "#f3e261";
 
-  ctx.lineTo(
-    e.clientX - canvasOffsetX,
-    (e.clientY - canvasOffsetY) + window.scrollY
-  );
+  const canvasOffsetX = canvas.offsetLeft;
+  const canvasOffsetY = canvas.offsetTop;
+  const x = e.clientX - canvasOffsetX;
+  const y = e.clientY - canvasOffsetY + window.scrollY;
+
+  ctx.lineTo(x, y);
   ctx.stroke();
 };
 
 canvas.addEventListener("mousedown", (e) => {
   isPainting = true;
-  startX = e.clientX - canvasOffsetX;
-  startY = e.clientY;
+  const canvasOffsetX = canvas.offsetLeft;
+  const canvasOffsetY = canvas.offsetTop;
+  const x = e.clientX - canvasOffsetX;
+  const y = e.clientY - canvasOffsetY + window.scrollY;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
 });
 
 canvas.addEventListener("mouseup", (e) => {
   isPainting = false;
-  ctx.stroke();
-  ctx.beginPath();
 });
 
 canvas.addEventListener("mousemove", draw);
--- js/panorama.js
+++ js/panorama.js
@@ -2,15 +2,17 @@ function getImg() {
   const panorama = document.getElementById("panorama");
   const imgRange = document.getElementById("imgRange");
   imgRange.addEventListener("input", function () {
-    const value = this.value;
-    panorama.style.transform = "translateX(" + value + "px)";
+    const value = parseInt(this.value);
+    if (!isNaN(value)) {
+      panorama.style.transform = "translateX(" + value + "px)";
+    }
   });
 }
 
 getImg();
 
 const resolutions = [
-  { width: 320, min: -2500, max: 0 },
+  { width: 425, min: -1000, max: 0 },
   { width: 768, min: -2000, max: 0 },
   { width: 1244, min: -1525, max: 0 },
   { width: Infinity, min: -1525, max: 0 },
@@ -19,7 +21,8 @@ const resolutions = [
 window.addEventListener("resize", function () {
   const screenWidth = window.innerWidth;
   const rangeElement = document.getElementById("imgRange");
-  const { min, max } = resolutions.find((r) => screenWidth <= r.width);
+  const { min = 0, max = 0 } =
+    resolutions.find((r) => screenWidth <= r.width) || {};
 
   rangeElement.setAttribute("min", min);
   rangeElement.setAttribute("max", max);
